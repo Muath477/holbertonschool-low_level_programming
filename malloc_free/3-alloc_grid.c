644ced65dd82ad31734edd65e310b63e6fc44b86
Muath477 <moathalmuatiri@gmail.com>
2026-07-21 07:22:32 +0000

    Fix alloc_grid Betty line count

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdlib.h>
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
@@ -13,45 +11,25 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i;
-	int j;
+        int **grid;
+        int i;
+        int j;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
+        if (width <= 0 || height <= 0)
+                return (NULL);
 
-	grid = malloc(sizeof(int *) * height);
-	if (grid == NULL)
-		return (NULL);
+        grid = malloc(sizeof(int *) * height);
+        if (grid == NULL)
+                return (NULL);
 
-	i = 0;
-	while (i < height)
-	{
-		grid[i] = malloc(sizeof(int) * width);
-		if (grid[i] == NULL)
-		{
-			j = 0;
-			while (j < i)
-			{
-				free(grid[j]);
-				j++;
-			}
-			free(grid);
-			return (NULL);
-		}
-		i++;
-	}
-
-	i = 0;
-	while (i < height)
-	{
-		j = 0;
-		while (j < width)
-		{
-			grid[i][j] = 0;
-			j++;
-		}
-		i++;
-	}
-	return (grid);
-}
+        i = 0;
+        while (i < height)
+        {
+                grid[i] = malloc(sizeof(int) * width);
+                if (grid[i] == NULL)
+                {
+                        j = 0;
+                        while (j < i)
+                        {
+                                free(grid[j]);
+                                j+  
