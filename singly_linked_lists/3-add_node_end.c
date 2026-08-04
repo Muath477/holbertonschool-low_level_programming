8218c4d469af6eeb88441f410593ebe4e5ebca04
Muath477 <moathalmuatiri@gmail.com>
2026-08-04 20:48:52 +0300

    Fix add_node and add_node_end to not use strlen

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,6 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *temp;
+	unsigned int len;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -25,7 +26,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
